c18d678f89d1b85791a453d9936e084ee1f757b1
hoodwinksolo <suharfeed@gmail.com>
2024-05-13 16:26:55 +0600

    функционал MyBooks

--- app/src/main/java/com/example/boook_sale/MyBooksActivity.java
+++ app/src/main/java/com/example/boook_sale/MyBooksActivity.java
@@ -2,7 +2,6 @@ package com.example.boook_sale;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,9 +12,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MyBooksActivity extends AppCompatActivity {
     private ListView listView;
@@ -33,26 +30,21 @@ public class MyBooksActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myBooks);
         listView.setAdapter(adapter);
 
-        if (getIntent().getBooleanExtra("updateNeeded", false)) {
-            loadMyBooks();  // Загрузка или обновление списка книг
-        }
+        loadMyBooks();
     }
 
     private void loadMyBooks() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
             db.collection("Users").document(user.getUid()).collection("MyBooks")
                     .get()
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
-                            List<Book> fetchedBooks = new ArrayList<>();
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Book book = document.toObject(Book.class);
-                                fetchedBooks.add(book);
+                                myBooks.add(book);
                             }
-                            // Обновление UI
-                            updateBooksList(fetchedBooks);
+                            adapter.notifyDataSetChanged();
                         } else {
                             Toast.makeText(this, "Error loading books: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -61,16 +53,8 @@ public class MyBooksActivity extends AppCompatActivity {
             Toast.makeText(this, "Please log in to view your books", Toast.LENGTH_LONG).show();
         }
     }
-
-
-
-    private void updateBooksList(List<Book> books) {
-        myBooks.clear();
-        myBooks.addAll(books);
-        adapter.notifyDataSetChanged(); // Убедитесь, что адаптер подключен к ListView или RecyclerView
-    }
-
 }
+
 
 
 
